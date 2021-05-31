de48fd973dcbf2d907103e8fed9ef46e4738eee5
VimukthiHewage <85064432+VimukthiHewage@users.noreply.github.com>
2021-05-31 14:33:17 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,27 @@
+import sys
+import os
+os.system ("clear")
+#A Tool By Vimukthi Hewage
+#DON'T COPYWRIGHT.
+
+print ("\033[1;33m        ____ ____      _    ____  _____")
+print ("\033[1;33m       / ___|  _ \    / \  |  _ \| ____|")
+print ("\033[1;33m      | |  _| |_) |  / _ \ | | | |  _|")
+print ("\033[1;33m      | |_| |  _ <  / ___ \| |_| | |___")
+print ("\033[1;33m       \____|_| \_\/_/   \_\____/|_____|")
+print ("\033[1;36m          A Tool By Vimukthi Hewage")
+print ("\033[1;36m          St.Anthony's College Kandy")
+
+marks=int(input("\033[1;33m enter your marks:"))
+if marks > 100:
+  print ("\033[1;31m they are no marks grade")
+elif marks >= 75 :
+  print ("\033[1;32m your grade is A")
+elif marks >= 65 :
+  print ("\033[1;32m your grade is B")
+elif marks >= 55 :
+  print ("\033[1;34m your grade is C")
+elif marks >= 35 :
+  print ("\033[1;35m your grade S")
+else :
+  print ("\033[1;31m you are Fail")
